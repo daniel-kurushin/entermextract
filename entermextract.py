89ef96099aaf4a960e5151ddae46b67d30ccd43f
daniel-kurushin <daniel.kurushin@gmail.com>
2021-06-16 21:00:40 +0500

    text from brown

--- entermextract.py
+++ entermextract.py
@@ -1,166 +1,12 @@
 from nltk.tokenize import WordPunctTokenizer as WPT
 from nltk.corpus import stopwords as SW
+from nltk.corpus import brown
 
 stopwords = set(SW.words('english'))
 
 wpt = WPT()
-# special thanks to Yandex.Referats
-corpora_text = '''The integral over an infinite domain, without going into details, is not
-uninterestingly corresponding to the parallel maximum, which will undoubtedly
-lead us to the truth. Until recently, it was assumed that a proper subset
-specifies the collinear rotor of a vector field. Higher arithmetic unwinds the
-curvilinear integral, which will undoubtedly lead us to the truth. If we
-assume that a < b, then the subset is unpredictable. Due to the continuity of
-the function f ( x), the geodesic line elegantly reflects an increasing power
-series. Multiplication of two vectors (vector) positively reflects the jump of
-the function. Continuing to infinity the row 1, 2, 3, 5, 7, 11, 13, 17, 19,
-23, 29, 31 and so on, we have a double integral that accelerates the natural
-logarithm. It is not a fact that the natural logarithm neutralizes the
-irrefutable polynomial. The Dirichlet integral projects the extremum of the
-function, from which the proved equality follows. The relative error,
-excluding the obvious case, is positive. The origin actually inverts the
-counterexample, which will undoubtedly lead us to the truth. Unique, capacious
-slogans, composed by the on-duty robot-criator – an indispensable assistant in
-a creative crisis. Just enter the name of the company or product, and the
-robot will do everything for you. Yandex allows you to use the robot's
-compositions in your advertising campaigns. Make a question. Enter the keyword
-of this question (name, event, etc.) in the line, click "find out" and get a
-concise answer. Everyone will have to decipher the messages themselves, since
-the same words have different meanings for different people.The Newton
-binomial, excluding the obvious case, stabilizes the positive integral over
-the oriented domain. The function convex upward, \ ha0consequently, stabilizes
-the abnormal maximum, clearly demonstrating all the nonsense of the above. An
-indefinite integral essentially positions an absolutely convergent series. The
-Bernoulli inequality in principle specifies a multidimensional counterexample.
-The geodesic line, \ ha0, obviously defines the trigonometric integral of a
-function that goes to infinity at an isolated point. The normal distribution
-attracts the integral of the function that has a finite gap. The vector field,
-\ ha0, terminally, sequentially recovers the Greatest Common Divisor (GCD).
-The sum of the series attracts the integral of the function that goes to
-infinity along the line. The lemma strengthens the power series, which was
-required to be proved. The closed set, \ ha0 is finite, directly creates the
-determinant of a system of linear equations, which will undoubtedly lead us to
-the truth. An absolutely convergent series stabilizes the integral of a
-function of a complex variable. Linear programming consistently specifies the
-axiomatic limit of the sequence, from which the proven equality follows. A
-complex number covers an integral over an infinite domain. Multiplication of
-two vectors (scalar) resolutely scales the tending rotor of the vector field.
-You can \ xa0position,\xa0that the vector field rotor categorically programs
-the Fourier integral. Unique, capacious slogans, composed by the on-duty
-robot-criator – an indispensable assistant in a creative crisis. Just enter
-the name of the company or product, and the robot will do everything for you.
-Yandex allows you to use the robot's compositions in your advertising
-campaigns. Make a question. Enter the keyword of this question (name, event,
-etc.) in the line, click "find out" and get a concise answer. Everyone will
-have to decipher the messages themselves, since the same words have different
-meanings for different people.The geometric progression causes the Newton
-binomial. The differential calculus, \ ha0 as\ha0 follows\ha0 from\ha0 above,
-uniformly transforms the normal rotor of a vector field. According to \
-ha0present, the primitive function consistently causes a positive
-discontinuity of the function. The orthogonal determinant synchronizes the
-divergent series, and we will leave the further calculations to the students
-as a simple homework. The scalar field, \ ha0 is generally known, positively
-scales the real determinant. Due to the continuity of the function f ( x), an
-absolutely convergent series is nontrivial. The asymptote uniformly generates
-an orthogonal determinant. The integral of a function having a finite
-discontinuity is based on \ ha0n\ha0pyth. The Gauss - Ostrogradsky theorem
-transforms the postulate, which is known even to schoolchildren. The integral
-of a function that goes to infinity along the line is normally distributed.
-Consider a continuous function y = f (x) given on the segment [ a, b ], the
-asymptote strengthens the real graph of the function. Piecemeal integration is
-unprovable. The proper subset scales the axiomatic integral over the surface.
-Mathematical modeling unambiguously shows that a rational number is
-consistent. Unique, capacious slogans, composed by the on-duty robot-criator –
-an indispensable assistant in a creative crisis. Just enter the name of the
-company or product, and the robot will do everything for you. Yandex allows
-you to use the robot's compositions in your advertising campaigns. Make a
-question. Enter the keyword of this question (name, event, etc.) in the line,
-click "find out" and get a concise answer. Everyone will have to decipher the
-messages themselves, since the same words have different meanings for
-different people.Epsilon neighborhood spins an incredible Mobius sheet. From
-here\ha0 naturally \ ha0 follows,\ha0 that a connected set specifies an
-equally probable limit of the sequence. Due to the continuity of the function
-f ( x), the geodesic line is determined. Multiplying the vector by a number
-stabilizes the positive Taylor series. The graph of a function of many
-variables is irrational. The origin transforms the abnormal natural logarithm.
-You can \ ha0position,\ha0 that the closed set orders the minimum, as
-expected. However, not everyone knows that the number e produces an equally
-probable vector. Corollary: The Newton binomial specifies a negative
-orthogonal determinant. The integer, \ ha0 is generally known, strongly
-determines the empirical method of successive approximations. Despite the
-complexity, the closed set determines the graph of the function. The imaginary
-unit, in the first approximation, tends to zero. Investigation: the epsilon
-neighborhood is negative. The polynomial unwinds a divergent series. Unique,
-capacious slogans, composed by the on-duty robot-criator – an indispensable
-assistant in a creative crisis. Just enter the name of the company or product,
-and the robot will do everything for you. Yandex allows you to use the robot's
-compositions in your advertising campaigns. Make a question. Enter the keyword
-of this question (name, event, etc.) in the line, click "find out" and get a
-concise answer. Everyone will have to decipher the messages themselves, since
-the same words have different meanings for different people.Fermat's theorem
-is degenerate. The interpolation, \ x0consistently, uniformly synchronizes the
-irrefutable integral of a function that goes to infinity at an isolated point.
-Starting with the proof, it should be stated categorically that Fermat's
-theorem attracts functional analysis, clearly demonstrating all the nonsense
-of the above. The integral of a function that goes to infinity along a line is
-irrational. If we assume that a < b, then the geodesic line strongly specifies
-the multidimensional Dirichlet integral. The fact is that the largest and
-smallest values of the function are necessary and sufficient. Interpolation
-programs an irrefutable power series. However, not everyone knows that the
-coordinate system attracts the decreasing Hamilton integral, and we will leave
-the further calculations to the students as a simple homework. A closed set
-transforms an empirical indefinite integral. So, it is clear that the
-irrational number balances the collinear Dirichlet integral. Linear
-programming, \ ha0 is obvious, reconstructs the integral of a function that
-has a finite discontinuity. The expectation is not critical. Mathematical
-modeling unambiguously shows that the subset is unprovable. The normal
-distribution is striking. Starting with the proof, we should categorically
-state that the Poisson integral programs a multidimensional orthogonal
-determinant, and we will leave the further calculations to the students as a
-simple homework. The partial differential equation, in the first
-approximation, projects the empirical Mobius sheet, and as a result we come to
-a logical contradiction. The minimum changes the irrefutable double integral,
-which is what we needed to prove. The epsilon neighborhood balances the
-indefinite integral. Unique, capacious slogans, composed by the on-duty
-robot-criator – an indispensable assistant in a creative crisis. Just enter
-the name of the company or product, and the robot will do everything for you.
-Yandex allows you to use the robot's compositions in your advertising
-campaigns. Make a question. Enter the keyword of this question (name, event,
-etc.) in the line, click "find out" and get a concise answer. Everyone will
-have to decipher the messages themselves, since the same words have different
-meanings for different people.The jump of the function, \ ha0, obviously
-concentrates the curvilinear integral, and as a result we come to a logical
-contradiction. The length of the vector is obvious\ha0ne\ha0for\ha0ex. The
-Bernoulli inequality is monotonic. The Bernoulli inequality, \ ha0 is
-generally known, justifies the experimental convergent series, thus the
-idiot's dream came true - the statement is fully proven. A proper subset
-develops a polynomial. An irrational number positively neutralizes the
-trigonometric limit of the function. The normal to the surface,\ha0 as\ha0
-follows\ha0 from\ha0 above, is negative. The determinant of a system of linear
-equations reflects the trigonometric curvilinear integral. The graph of the
-function meaningfully determines the incredible Dirichlet integral. The Axiom
-consistently determines the Greatest Common Divisor (GCD), which is known even
-to schoolchildren. The graph of the function uniformly corresponds to the
-complex jump of the function. The field of directions, \ ha0 is sequential,
-traditionally reflects a power series.'''.lower()
 
-a_text = """Multiplying a vector by a number is therefore trivial. The length of the
-vector is still in demand. It is not proven that the Mobius sheet rapidly
-positions the graph of the function.
-
-The neighborhood of the point unwinds the parallel triple integral, which will
-undo7ubtedly lead us to the truth. The open set essentially determines the
-Dirichlet integral, from which the proved equality follows. The determinant of
-a system of linear equations orders the determinant of a system of linear
-equations, which will undoubtedly lead us to the truth.
-
-The triple integral, excluding the obvious case, transforms the empirical
-postulate. A closed set, therefore, uniformly transforms the normal
-determinant. Higher arithmetic organizes mathematical analysis. Higher
-arithmetic defines the positive integral of a function of a complex variable.
-The Fourier integral is ambiguous.""".lower()
-
-b_text = '''An absolutely convergent series, as follows from the above, is proved. In
+a_text = '''An absolutely convergent series, as follows from the above, is proved. In
 general, the sum of the series strengthens the integral over the surface, from
 which the equality to be proved follows. The counterexample attracts the
 empirical Mobius leaf, thus the idiot's dream came true - the statement is
@@ -187,7 +33,7 @@ r_text = '''Абсолютно сходящийся ряд, как следуе�
 class TermExtractor():
     
     def __init__(self):
-        the_corpora = [ x for x in wpt.tokenize(corpora_text) if x not in set(".,*+=-'\"/\!1234567890()[]") and x not in stopwords ]
+        the_corpora = [ x for x in brown.words()[:100000] if x not in set(".,*+=-'\"/\!1234567890()[]") and x not in stopwords ]
         frequrencies = []
 
         for n in range(1,5):
